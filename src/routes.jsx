9bb709da38a37a5654f496a006a50fb428964632
Your Name <mjahdkaran123@gmail.com>
2024-12-05 21:27:46 +0330

    Fix base in vite config

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,7 +5,7 @@ import MovieDetails from './Pages/MovieDetails/MovieDetails';
 export default function routes() {
     return (
         <Routes>
-            <Route path='/movieapp/' element={<Movies />} />
+            <Route path='/movieapp' element={<Movies />} />
             <Route path="/movieapp/m/:id" element={<MovieDetails />} />
         </Routes>
     )
