e20f7bed5d8902174f38ce24924cdb2a36606db3
Your Name <mjahdkaran123@gmail.com>
2025-03-02 21:05:24 +0330

    add loading for links

--- src/Components/DownLoadLinks/DownLoadLinks.jsx
+++ src/Components/DownLoadLinks/DownLoadLinks.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function DownLoadLinks({ downloadLinksArray }) {
     const [copyMessage, setCopyMessage] = useState(false);
     const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+    const [isLoading, setIsLoading] = useState(false)
 
+    useEffect(() => {
+        downloadLinksArray.length > 0 ? setIsLoading(false) : setIsLoading(true)
+    })
     // مرتب‌سازی کیفیت از کم به زیاد
     const sortedLinks = [...downloadLinksArray].sort((a, b) => parseInt(a.quality) - parseInt(b.quality));
 
@@ -26,26 +30,29 @@ export default function DownLoadLinks({ downloadLinksArray }) {
 
     return (
         <div className='p-1 relative'>
-            <ul className='flex flex-wrap justify-start md:justify-between text-gray- text-xs'>
-                {sortedLinks.map((link, index) => (
-                    <li
-                        onClick={(e) => handleLinkClick(e, link.link)}
-                        key={index}
-                        className="w-fit bg-pink-800 md:p-2 p-1 rounded-lg hover:bg-pink-900 m-1 md:min-w-72 transition-colors duration-500 relative"
-                    >
-                        <a
-                            href={link.link}
-
-                            className="cursor-pointer block"
+            {isLoading ? <p className='text-white font-bold flex justify-center p-2 '>is Loading...</p> :
+                <ul className='flex flex-wrap justify-start md:justify-between text-gray- text-xs'>
+                    {sortedLinks.map((link, index) => (
+                        <li
+                            onClick={(e) => handleLinkClick(e, link.link)}
+                            key={index}
+                            className="w-fit bg-pink-800 md:p-2 p-1 rounded-lg hover:bg-pink-900 m-1 md:min-w-72 transition-colors duration-500 relative"
                         >
-                            <span className='md:inline hidden'>DownLoad - </span>
-                            <span className='font-bold text-white'> {link.quality} </span>
-                            - {link.type} -
-                            <span className='font-bold text-white'> {link.size} </span>
-                        </a>
-                    </li>
-                ))}
-            </ul>
+                            <a
+                                href={link.link}
+
+                                className="cursor-pointer block"
+                            >
+                                <span className='md:inline hidden'>DownLoad - </span>
+                                <span className='font-bold text-white'> {link.quality} </span>
+                                - {link.type} -
+                                <span className='font-bold text-white'> {link.size} </span>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            }
+
 
             {/* پیام کوچک "Copy" که کنار موس نمایش داده می‌شود */}
             {copyMessage && (
